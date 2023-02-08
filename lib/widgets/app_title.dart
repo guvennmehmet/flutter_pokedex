110ba01d51aec328700fe365efa7405de601ab61
guvennmehmet <guvennmehmet37@gmail.com>
2023-02-08 13:26:26 +0300

    appTitle widgetının bitirilmesi

--- lib/widgets/app_title.dart
+++ lib/widgets/app_title.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_pokedex/constants/constants.dart';
+import 'package:flutter_pokedex/constants/ui_helper.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class AppTitle extends StatefulWidget {
@@ -14,22 +15,30 @@ class _AppTitleState extends State<AppTitle> {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Align(
-          alignment: Alignment.topLeft,
-          child: Text(Constants.title, style: Constants.getTitleTextStyle()),
-        ),
-        Align(
-          alignment: Alignment.topRight,
-          child: Image.asset(
-            pokeballImageUrl,
-            width: 100.w,
-            height: 100.w,
-            fit: BoxFit.fitWidth,
+    return SizedBox(
+      height: UIHelper.getAppTitleWidgetHeight(),
+      child: Stack(
+        children: [
+          Padding(
+            padding: UIHelper.getDefaultPadding(),
+            child: Align(
+              alignment: Alignment.centerLeft,
+              child:
+                  Text(Constants.title, style: Constants.getTitleTextStyle()),
+            ),
           ),
-        )
-      ],
+          Align(
+            alignment: Alignment.topRight,
+            child: Image.asset(
+              pokeballImageUrl,
+              width: ScreenUtil().orientation == Orientation.portrait
+                  ? 0.2.sh
+                  : 0.2.sw,
+              fit: BoxFit.fitWidth,
+            ),
+          )
+        ],
+      ),
     );
   }
 }
