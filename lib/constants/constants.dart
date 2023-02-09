f09d931f5ad47342d7a5d6752f61caa81116c0cd
guvennmehmet <guvennmehmet37@gmail.com>
2023-02-09 14:32:07 +0300

    orientation builder kullanılması ve listitem widget

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -11,4 +11,15 @@ class Constants {
     return const TextStyle(
         color: Colors.white, fontWeight: FontWeight.bold, fontSize: 48);
   }
+
+  static TextStyle getPokemonNameTextStyle() {
+    return const TextStyle(
+        color: Colors.white, fontWeight: FontWeight.bold, fontSize: 30);
+  }
+
+  static TextStyle getTypeChipTextStyle() {
+    return const TextStyle(
+        color: Colors.white, fontSize: 20);
+  }
+
 }
--- lib/widgets/pokemon_list.dart
+++ lib/widgets/pokemon_list.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_pokedex/widgets/pokelist_item.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import '../model/pokemon_model.dart';
 import '../services/pokedex_api.dart';
 
@@ -25,15 +27,12 @@ class _PokemonListState extends State<PokemonList> {
       builder: (context, snapshot) {
         if (snapshot.hasData) {
           List<PokemonModel> _listem = snapshot.data!;
-          return ListView.builder(
-            itemCount: _listem.length,
-            itemBuilder: (context, index) {
-              var oankiPokemon = _listem[index];
-              return ListTile(
-                title: Text(oankiPokemon.name.toString()),
-              );
-            },
-          );
+          return GridView.builder(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount:
+                      ScreenUtil().orientation == Orientation.portrait ? 2 : 3),
+              itemBuilder: (context, index) =>
+                  PokeListItem(pokemon: _listem[index]));
         } else if (snapshot.hasError) {
           return const Center(
             child: Text('Hata Çıktı'),
