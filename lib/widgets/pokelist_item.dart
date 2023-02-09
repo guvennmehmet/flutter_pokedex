e642e590542f52245c174e8b690dbfad51fe1c43
guvennmehmet <guvennmehmet37@gmail.com>
2023-02-09 14:41:39 +0300

    ozet ve yazi fontlarinin responsive yapilmasi

--- lib/widgets/pokelist_item.dart
+++ lib/widgets/pokelist_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_pokedex/constants/ui_helper.dart';
 import 'package:flutter_pokedex/model/pokemon_model.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import '../constants/constants.dart';
@@ -17,14 +18,19 @@ class PokeListItem extends StatelessWidget {
       elevation: 3,
       shadowColor: Colors.white,
       color: Colors.red.shade200,
-      child: Column(
-        children: [
-          Text(
-            pokemon.name ?? 'N/A',
-            style: Constants.getPokemonNameTextStyle(),
-          ),
-          Chip(label: Text(pokemon.type![0]))
-        ],
+      child: Padding(
+        padding: UIHelper.getDefaultPadding(),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              pokemon.name ?? 'N/A',
+              style: Constants.getPokemonNameTextStyle(),
+            ),
+            Chip(label: Text(pokemon.type![0]))
+          ],
+        ),
       ),
     );
   }
